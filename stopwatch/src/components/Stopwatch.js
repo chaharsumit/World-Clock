514f5fcf26dabfa737ac0242309cfa0b7514f38f
Sumit Chahar <chahar.sumit888@gmail.com>
2022-01-28 12:52:23 +0530

    Create UI for countdown and stopwatch

---
+++ stopwatch/src/components/Stopwatch.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+class Stopwatch extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      show: false
+    }
+  }
+
+  handleClick = () => {
+    this.setState((prevState) => {
+      return {
+        show: !prevState.show
+      }
+    })
+  }
+
+  handleClose = () => {
+    this.setState((prevState) => {
+      return {
+        show: !prevState.show
+      }
+    })
+  }
+
+  render(){
+    return (
+      <>
+        {
+          !this.state.show
+          ?
+            <button className='btn' onClick={this.handleClick}>Show Stopwatch</button>
+          :
+            <div className='watch'>
+              <h2>StopWatch</h2>
+              <div className='time-container'>
+                <span>00 :</span>
+                <span> 00 :</span>
+                <span> 00 :</span>
+                <span> 00</span>
+              </div>
+              <button className='btn-watch'>Start</button>
+              <span onClick={this.handleClose} className='cross'>X</span>
+            </div>
+        }
+      </>
+    )
+  }
+}
+
+export default Stopwatch;
